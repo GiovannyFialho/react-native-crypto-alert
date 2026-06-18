925adaf9c1695c508958615e47f3de9bd0e19fb3
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-06-17 21:01:40 -0300

    writing formatPrice test

---
+++ src/utils/formatTime.test.ts
@@ -0,0 +1,23 @@
+import { formatPrice } from "@/utils";
+
+describe("formatPrice", () => {
+  it("should format a price to 2 decimal places", () => {
+    const formattedPrice = formatPrice(1000);
+    expect(formattedPrice).toBe("1,000.00");
+  });
+
+  it("should round correctly", () => {
+    const formattedPrice = formatPrice(1000.121);
+    expect(formattedPrice).toBe("1,000.12");
+  });
+
+  it("should handle 0 correctly", () => {
+    const formattedPrice = formatPrice(0);
+    expect(formattedPrice).toBe("0.00");
+  });
+
+  it("should handle negative prices correctly", () => {
+    const formattedPrice = formatPrice(-1000);
+    expect(formattedPrice).toBe("-1,000.00");
+  });
+});
