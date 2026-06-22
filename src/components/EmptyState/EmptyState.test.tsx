d1a621c8a81f9cbaf5df5dc0b01f72d5b2014e93
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-06-21 21:44:41 -0300

    creating test using testing-library for empty-state component

---
+++ src/components/EmptyState/EmptyState.test.tsx
@@ -0,0 +1,44 @@
+import { render } from "@testing-library/react-native";
+import React from "react";
+import { Text } from "react-native";
+
+import { EmptyState } from "@/components/EmptyState/EmptyState";
+
+describe("EmptyState", () => {
+  it("should render the title and description", async () => {
+    const title = "Nenhum alert encontrado";
+    const description =
+      "Crie seu primeiro alerta para ser notificado quando uma criptomoeda atingir seu preço alvo.";
+    const icon = "🚀";
+
+    const { getByText } = await render(
+      <EmptyState
+        icon={<Text>{icon}</Text>}
+        title={title}
+        description={description}
+      />,
+    );
+
+    expect(getByText(icon)).toBeTruthy();
+    expect(getByText(title)).toBeTruthy();
+    expect(getByText(description)).toBeTruthy();
+  });
+  it("should render the action button", async () => {
+    const title = "Nenhum alert encontrado";
+    const description =
+      "Crie seu primeiro alerta para ser notificado quando uma criptomoeda atingir seu preço alvo.";
+    const icon = "🚀";
+    const action = <Text>Criar alerta</Text>;
+
+    const { getByText } = await render(
+      <EmptyState
+        icon={<Text>{icon}</Text>}
+        title={title}
+        description={description}
+        action={<Text>{action}</Text>}
+      />,
+    );
+
+    expect(getByText("Criar alerta")).toBeTruthy();
+  });
+});
