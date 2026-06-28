ab5814cb68e70fdda61fa48f7205fecdcd17b887
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-06-28 13:34:48 -0300

    creating crypto-data test

---
+++ src/data/cryptoData.test.ts
@@ -0,0 +1,19 @@
+import {
+  cryptocurrencies,
+  fetchCryptocurrenciesFromApi,
+} from "@/data/cryptoData";
+
+describe("API: CryptoData", () => {
+  it("should fetch cryptocurrencies from the API", async () => {
+    jest.spyOn(globalThis, "fetch").mockResolvedValue({
+      ok: true,
+      json: async () => cryptocurrencies,
+    } as Response);
+
+    const data = await fetchCryptocurrenciesFromApi();
+
+    expect(data).toBeDefined();
+    expect(Array.isArray(data)).toBe(true);
+    expect(data.length).toBe(cryptocurrencies.length);
+  });
+});
