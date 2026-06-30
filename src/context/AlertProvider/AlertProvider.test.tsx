217466c3d353ec51ca0b7369e915f609e03dc222
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-06-30 18:22:43 -0300

    adjusting tests

--- src/context/AlertProvider/AlertProvider.test.tsx
+++ src/context/AlertProvider/AlertProvider.test.tsx
@@ -1,16 +1,40 @@
-import { render } from "@testing-library/react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { render, waitFor } from "@testing-library/react-native";
 import { Text } from "react-native";
 
 import { AlertProvider } from "@/context/AlertProvider/AlertProvider";
+import { cryptoAlertsMock } from "@mocks/storage/cryptoAlerts";
 
 describe("Context: AlertProvider", () => {
   it("should render the children", async () => {
-    const { debug } = await render(
+    jest.spyOn(AsyncStorage, "getItem").mockResolvedValue(null);
+
+    const { getByText } = await render(
       <AlertProvider>
         <Text>Hello, world!</Text>
       </AlertProvider>,
     );
 
-    debug();
+    await waitFor(() => {
+      expect(AsyncStorage.getItem).toHaveBeenCalled();
+    });
+
+    expect(getByText("Hello, world!")).toBeTruthy();
+  });
+
+  it("should call AsyncStorage.getItem when the component is mounted", async () => {
+    const asyncStorageGetItem = jest
+      .spyOn(AsyncStorage, "getItem")
+      .mockResolvedValue(JSON.stringify(cryptoAlertsMock));
+
+    render(
+      <AlertProvider>
+        <Text>Hello, world!</Text>
+      </AlertProvider>,
+    );
+
+    await waitFor(async () => {
+      expect(asyncStorageGetItem).toHaveBeenCalledWith("cryptoAlerts");
+    });
   });
 });
