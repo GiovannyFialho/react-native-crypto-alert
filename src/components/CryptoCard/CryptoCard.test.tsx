1672fb9d8661b24bc90ebafeeea95e20b9c4c9c5
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-07-01 18:59:32 -0300

    mocking functions from utils

--- src/components/CryptoCard/CryptoCard.test.tsx
+++ src/components/CryptoCard/CryptoCard.test.tsx
@@ -2,8 +2,12 @@ import { render } from "@testing-library/react-native";
 
 import { CryptoCard } from "@/components/CryptoCard/CryptoCard";
 import { AlertProvider } from "@/context/AlertProvider/AlertProvider";
-import { formatPrice } from "@/utils";
 import { cryptoCurrenciesMock } from "@mocks/data/cryptoCurrencies";
+
+jest.mock("@utils/index", () => ({
+  formatPrice: jest.fn().mockReturnValue("1,000.00"),
+  formatChange: jest.fn().mockReturnValue("+10.00%"),
+}));
 
 describe("Component: CryptoCard", () => {
   it("should render the crypto name, symbol and price", async () => {
@@ -15,7 +19,7 @@ describe("Component: CryptoCard", () => {
 
     const name = getByText(cryptoCurrenciesMock[0].name);
     const symbol = getByText(cryptoCurrenciesMock[0].symbol);
-    const price = getByText(`$${formatPrice(cryptoCurrenciesMock[0].price)}`);
+    const price = getByText(`$1,000.00`);
 
     expect(name).toBeTruthy();
     expect(symbol).toBeTruthy();
@@ -34,19 +38,7 @@ describe("Component: CryptoCard", () => {
     expect(avatar).toBeTruthy();
   });
 
-  it("should render negative change correctly", async () => {
-    const { getByText } = await render(
-      <AlertProvider>
-        <CryptoCard crypto={{ ...cryptoCurrenciesMock[0], change24h: -10 }} />
-      </AlertProvider>,
-    );
-
-    const change = getByText("-10.00%");
-
-    expect(change).toBeTruthy();
-  });
-
-  it("should render positive change correctly", async () => {
+  it("should render change correctly", async () => {
     const { getByText } = await render(
       <AlertProvider>
         <CryptoCard crypto={{ ...cryptoCurrenciesMock[0], change24h: 10 }} />
