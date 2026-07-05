16703072350c55e25fedc8cb5a3e55567eae8f62
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-07-05 13:13:45 -0300

    testing removeAlert

--- src/context/AlertProvider/useAlertProvider.test.ts
+++ src/context/AlertProvider/useAlertProvider.test.ts
@@ -19,4 +19,29 @@ describe("Context: AlertProvider", () => {
 
     expect(result.current.alerts).toHaveLength(1);
   });
+
+  it("should delete and alert", async () => {
+    const { result } = await renderHook(() => useAlertProviderState());
+
+    expect(result.current.alerts).toHaveLength(0);
+
+    await act(async () => {
+      result.current.addAlert({
+        cryptocurrency: "Bitcoin",
+        symbol: "BTC",
+        targetPrice: 100000,
+        condition: "above",
+      });
+    });
+
+    expect(result.current.alerts).toHaveLength(1);
+
+    const alertToDelete = result.current.alerts[0];
+
+    await act(async () => {
+      result.current.deleteAlert(alertToDelete.id);
+    });
+
+    expect(result.current.alerts).toHaveLength(0);
+  });
 });
