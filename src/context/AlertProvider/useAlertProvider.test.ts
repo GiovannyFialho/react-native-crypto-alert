a467cd588803de089b1debd823821b5536cec431
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-07-07 18:03:09 -0300

    tests for useAlertProvider

--- src/context/AlertProvider/useAlertProvider.test.ts
+++ src/context/AlertProvider/useAlertProvider.test.ts
@@ -1,6 +1,8 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { act, renderHook } from "@testing-library/react-native";
 
 import { useAlertProviderState } from "@/context/AlertProvider/useAlertProvider";
+import { cryptoAlertsMock } from "@mocks/storage/cryptoAlerts";
 
 describe("Context: AlertProvider", () => {
   it("should add an alert", async () => {
@@ -18,6 +20,8 @@ describe("Context: AlertProvider", () => {
     });
 
     expect(result.current.alerts).toHaveLength(1);
+
+    await AsyncStorage.clear();
   });
 
   it("should delete and alert", async () => {
@@ -43,5 +47,39 @@ describe("Context: AlertProvider", () => {
     });
 
     expect(result.current.alerts).toHaveLength(0);
+  });
+
+  it("should load alerts from AsyncStorage", async () => {
+    const getItemSpy = jest
+      .spyOn(AsyncStorage, "getItem")
+      .mockResolvedValue(JSON.stringify(cryptoAlertsMock));
+
+    const { result } = await renderHook(() => useAlertProviderState());
+
+    expect(result.current.alerts).toHaveLength(2);
+
+    getItemSpy.mockRestore();
+  });
+
+  it("should save alerts to AsyncStorage", async () => {
+    const { result } = await renderHook(() => useAlertProviderState());
+
+    expect(result.current.alerts).toHaveLength(0);
+
+    act(() => {
+      result.current.addAlert({
+        cryptocurrency: "Bitcoin",
+        symbol: "BTC",
+        targetPrice: 100000,
+        condition: "above",
+      });
+    });
+
+    expect(AsyncStorage.setItem).toHaveBeenCalledWith(
+      "cryptoAlerts",
+      JSON.stringify(result.current.alerts),
+    );
+
+    AsyncStorage.clear();
   });
 });
