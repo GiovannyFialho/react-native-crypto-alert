2d1ba03a827411c8daceacc3d9ada1399077145a
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-07-07 20:56:41 -0300

    compleating tests for useAlerts

--- src/context/AlertProvider/useAlerts.test.ts
+++ src/context/AlertProvider/useAlerts.test.ts
@@ -1,6 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { useAlerts } from "@/context/AlertProvider/useAlertProvider";
+import { cryptoAlertsMock } from "@mocks/storage/cryptoAlerts";
 import { act, renderHook } from "@tests/utils/test-utils";
 
 describe("Context: useAlerts", () => {
@@ -19,6 +20,65 @@ describe("Context: useAlerts", () => {
     });
 
     expect(result.current.alerts).toHaveLength(1);
+
+    await AsyncStorage.clear();
+  });
+
+  it("should delete and alert", async () => {
+    const { result } = await renderHook(() => useAlerts());
+
+    expect(result.current.alerts).toHaveLength(0);
+
+    await act(async () => {
+      result.current.addAlert({
+        cryptocurrency: "Bitcoin",
+        symbol: "BTC",
+        targetPrice: 100000,
+        condition: "above",
+      });
+    });
+
+    expect(result.current.alerts).toHaveLength(1);
+
+    const alertToDelete = result.current.alerts[0];
+
+    await act(async () => {
+      result.current.deleteAlert(alertToDelete.id);
+    });
+
+    expect(result.current.alerts).toHaveLength(0);
+  });
+
+  it("should load alerts from AsyncStorage", async () => {
+    const getItemSpy = jest
+      .spyOn(AsyncStorage, "getItem")
+      .mockResolvedValue(JSON.stringify(cryptoAlertsMock));
+
+    const { result } = await renderHook(() => useAlerts());
+
+    expect(result.current.alerts).toHaveLength(2);
+
+    getItemSpy.mockRestore();
+  });
+
+  it("should save alerts to AsyncStorage", async () => {
+    const { result } = await renderHook(() => useAlerts());
+
+    expect(result.current.alerts).toHaveLength(0);
+
+    act(() => {
+      result.current.addAlert({
+        cryptocurrency: "Bitcoin",
+        symbol: "BTC",
+        targetPrice: 100000,
+        condition: "above",
+      });
+    });
+
+    expect(AsyncStorage.setItem).toHaveBeenCalledWith(
+      "cryptoAlerts",
+      JSON.stringify(result.current.alerts),
+    );
 
     await AsyncStorage.clear();
   });
